189d04adf1dc015eedabbd43d6e41406a40fa776
Roberto Zazo Lopez <roberto.zazo.lopez@everis.com>
2015-11-03 17:29:39 +0100

    refactor | grid to list

--- src/proteo.ui.search-contact-directive.js
+++ src/proteo.ui.search-contact-directive.js
@@ -1,12 +1,7 @@
 angular.module("proteo.ui.search-contact", []).controller("searchContactCtrl",  function($scope) {
 		console.log("that is of Controller");
-
-		$scope.dataContact = {
-			data : 'datasource',
-			filterOptions : 'datafilter'
-		};
-
-}).directive("gridContact", function(){
+})
+/*.directive("gridContact", function(){
 	console.log("that is of Directive Grid");
 	return {
 		restrict: "AE",
@@ -15,6 +10,18 @@ angular.module("proteo.ui.search-contact", []).controller("searchContactCtrl",  
 			datasource 		: '=datasource',
 			datafilter 		: '=datafilter',
 			dataplaceholder : '=dataplaceholder'
+		},
+		controller : 'searchContactCtrl',
+		//template: template
+		templateUrl: './bower_components/proteo.ui.search-contact/src/proteo.ui.search-contact.tpl.html'
+	};
+});*/
+.directive("gridContact", function(){
+	console.log("that is of Directive Grid");
+	return {
+		restrict: "AE",
+		scope: {
+			datacontact : '=datacontact'
 		},
 		controller : 'searchContactCtrl',
 		//template: template
