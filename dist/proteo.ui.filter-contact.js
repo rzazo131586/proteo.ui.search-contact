d9d592cb12f5bbe6be34d45c5c208b389d32de30
Roberto Zazo Lopez <roberto.zazo.lopez@everis.com>
2015-11-04 12:49:25 +0100

    update DIST

---
+++ dist/proteo.ui.filter-contact.js
@@ -0,0 +1,35 @@
+angular.module("proteo.ui.search-contact", []).controller("searchContactCtrl",  function($scope) {
+		console.log("that is of Controller");
+})
+/*.directive("gridContact", function(){
+	console.log("that is of Directive Grid");
+	return {
+		restrict: "AE",
+		scope: {
+			datatitle 		: '=datatitle',
+			datasource 		: '=datasource',
+			datafilter 		: '=datafilter',
+			dataplaceholder : '=dataplaceholder'
+		},
+		controller : 'searchContactCtrl',
+		//template: template
+		templateUrl: './bower_components/proteo.ui.search-contact/src/proteo.ui.search-contact.tpl.html'
+	};
+});*/
+.directive("listContact", function(){
+	console.log("that is listContact");
+	return {
+		restrict: "AE",
+		scope: {
+			datacontact : '=datacontact'
+		},
+		controller : 'searchContactCtrl',
+		//template: template
+		templateUrl: './bower_components/proteo.ui.search-contact/dist/proteo.ui.search-contact.tpl.html'
+	};
+});
+angular.module("proteo.ui.search-contact", []).filter('searchContact-filter', function(){
+
+});
+angular.module("proteo.ui.search-contact", []);
+/* Controller of detail contact */
